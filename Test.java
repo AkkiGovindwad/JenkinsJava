12dfb74aaf329f42634316aa434b82f1ef9a8059
AkkiGovindwad <112241186+AkkiGovindwad@users.noreply.github.com>
2023-11-14 21:33:50 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class Test{
 
   public static void main(String args[]){
     System.out.println("This is Jenkins Java Program");
-    System.out.println("This is second line");
+    System.out.println("This is 2nd line");
   }
 }
